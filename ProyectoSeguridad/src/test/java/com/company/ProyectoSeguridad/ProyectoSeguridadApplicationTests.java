a8c06625bb937eeb4735586a9736a66def31a4b5
Diegosd75 <74265984+Diegosd75@users.noreply.github.com>
2024-10-21 15:28:22 -0500

    Add files via upload

---
+++ ProyectoSeguridad/src/test/java/com/company/ProyectoSeguridad/ProyectoSeguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.ProyectoSeguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSeguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
